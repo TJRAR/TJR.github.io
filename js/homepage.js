72f10543b7a3f5839dc91b7a20922f35e43b3a3b
TJRAR <66017961+TJRAR@users.noreply.github.com>
2020-10-01 09:28:50 +0800

    优化样式，将js写入.js

---
+++ js/homepage.js
@@ -0,0 +1,25 @@
+document.getElementById("list").style.visibility = "hidden";
+
+function fclist() {
+	setTimeout(function() {
+		document.getElementById("list").style.visibility = "visible";
+	}, 2000);
+}
+document.getElementById("ceil").style.visibility = "hidden";
+
+function fcceil() {
+	setTimeout(function() {
+		document.getElementById("ceil").style.visibility = "visible";
+	}, 1000);
+}
+divset = document.getElementsByClassName("postdiv");
+for (var i = 0; i < divset.length; i++) {
+	divset[i].style.visibility = "hidden";
+}
+
+function fcpostdiv() {
+	setTimeout(function() {
+		divset = document.getElementsByClassName("postdiv");
+		for (var i = 0; i < divset.length; i++) divset[i].style.visibility = "visible";
+	}, 1000)
+}
